d4c760fb4583c18d47743d6149213279362a1ab4
JahfarEv <evjahfarali@gmail.com>
2024-02-09 08:13:23 +0530

    auth

--- auth.js
+++ auth.js
@@ -1,38 +1,59 @@
-const { PrismaAdapter } = require("@auth/prisma-adapter");
-const { NextAuthOptions } = require("next-auth");
-const prisma = require('@/lib/prisma')
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
+const prisma = new PrismaClient()
 
-const config = {
-  pages: {
-    signIn: '/login'
-  },
+export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   session: {
-    strategy:"jwt",
+    strategy:'jwt'
   },
   callbacks: {
-    async session({session,token}){
+   async session({session,token}){
       if(token){
         session.user.id = token.id;
         session.user.name = token.name;
-        session.user.email = token.email;
         session.user.image = token.picture;
         session.user.username = token.username;
       }
-      return session
-    }
+      return session;
+    },
+     async jwt({token,user}){
+const prismaUser = await prisma.user.findFirst({
+  where:{
+    email:token.email
   }
-    
+})
+if(!prismaUser){
+  token.id = user.id
+  return token
+}
+if(!prismaUser.username){
+  await prisma.user.update({
+    where:{
+      id: prismaUser.id
+    },
+    data: {
+      username:prismaUser.name?.split(" ").join("").toLowerCase()
+    }
+  })
+}
+return {
+  id:prismaUser.id,
+  name:prismaUser.name,
+  email:prismaUser.email,
+  username:prismaUser.username,
+  picture:prismaUser.image
+}
+    }
 
-    
-};
-
-module.exports = { config, satisfies: NextAuthOptions };
+  }
+})

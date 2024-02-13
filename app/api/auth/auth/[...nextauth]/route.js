05fdd6741acc0b3a254c6291f30f64cf2f55c13c
JahfarEv <evjahfarali@gmail.com>
2024-02-13 10:56:39 +0530

    updated 1.2

--- app/api/auth/auth/[...nextauth]/route.js
+++
@@ -1,8 +0,0 @@
-// import {config} from "@/auth"
-// import NextAuth from "next-auth/next"
-
-// const handler = NextAuth({
-//  config
-// });
-
-// module.exports = { GET: handler, POST: handler };

0eb11256e7a9ab5d193727980f9379a9f8fb831e
JahfarEv <evjahfarali@gmail.com>
2024-02-17 06:32:58 +0530

    middleware updated

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
+import { UserButton } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 export default function page() {
-  return redirect("/");
+  return redirect("/dashbord");
   // <div className="h-screen">
   //   <UserButton afterSignOutUrl="/dashboard"/>
   // </div>
